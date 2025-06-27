b1e9f8e291997cb25bbc4a2b9ee1b75b21a58442
ThePolishCyan <natanieltany12@gmail.com>
2025-06-27 21:20:56 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -37,7 +37,6 @@ addLayer("p", {
             effect() {
                 return player[this.layer].points.add(1).pow(0.5)
             },
-            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
         },
     },
 
