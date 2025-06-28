25f3b82857dd0c746e536ab37ea17a27de60d47b
ThePolishCyan <natanieltany12@gmail.com>
2025-06-28 10:36:23 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,7 +14,8 @@ addLayer("p", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        let mult = new Decimal(1)
+        if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -39,7 +40,15 @@ addLayer("p", {
             effect() {
                 return player[this.layer].points.add(1).pow(0.5)
             },
+        },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+        13: {
+            title: "0D Gain I",
+            description: "Multiply your 0D points gain depending on your vectors.",
+            cost: new Decimal(4),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
         },
     },
 
